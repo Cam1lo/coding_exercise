c2eabe4be19c727ef6ac8f2d60894195aac7e3d6
Cam1lo <47703795+Cam1lo@users.noreply.github.com>
2024-04-01 20:37:03 -0400

    Removing hardcoded environment

--- core/api/hotels.ts
+++ core/api/hotels.ts
@@ -1,13 +1,13 @@
 export async function getHotels() {
     // await new Promise((resolve) => setTimeout(resolve, 1000));
 
-    return fetch(`http://localhost:3000/api/hotels`)
+    return fetch(process.env.API_URL + `/hotels`)
         .then(response => response.json())
         .then(data => data);
 }
 
 export async function getHotelById(id: string) {
-    return fetch(`http://localhost:3000/api/hotels/${id}`)
+    return fetch(process.env.API_URL + `/hotels/${id}`)
         .then(response => response.json())
         .then(data => data);
 }
